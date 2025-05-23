53abc977584036598f467378a4c16a8a3de8a4fc
Luiz Felipe Escandiuzzi <lfescandiuzzi@icloud.com>
2025-05-23 11:35:37 -0300

    feat: frontend improvements

--- tempotec/routes.py
+++ tempotec/routes.py
@@ -41,6 +41,13 @@ def login():
                 return redirect(url_for('home'))
         else:
             flash(f'Falha no Login. E-mail ou Senha Incorretos', 'alert-danger')
+    return render_template('login.html', form_login=form_login)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
         senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
         usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha_cript)
@@ -48,7 +55,7 @@ def login():
         database.session.commit()
         flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
         return redirect(url_for('home'))
-    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
+    return render_template('register.html', form_criarconta=form_criarconta)
 
 
 @app.route('/sair')
